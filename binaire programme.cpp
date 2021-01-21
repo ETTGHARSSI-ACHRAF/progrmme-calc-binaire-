79239d6bd3f16060ce385076693ddfa72add8096
abdelbarre dazia <abdelbarredazia@gmail.com>
2021-01-21 13:56:19 +0100

    final

--- binaire programme.cpp
+++ binaire programme.cpp
@@ -1,11 +1,11 @@
 #include<stdio.h>
-
+#include <math.h>
 
 //la function de calcule la somme de 2 binaire
 // resulta de type int
 int Somme(int bin1[4],int bin2[4]){
 
-	
+
 }
 
 
@@ -13,7 +13,7 @@ int Somme(int bin1[4],int bin2[4]){
 /*function type INT*/
 
 int bin(){
-	
+
 }
 
 
@@ -21,17 +21,29 @@ int bin(){
 // fonction int
 // get int (biin)
 // return int (_innt)
-int bintoint (int biin)
-{
-    int _innt;
 
-    return _innt;
+int binaryToDecimal(int binarynum)
+{
+    int decimalnum = 0, temp = 0, remainder;
+    while (binarynum!=0)
+    {
+        remainder = binarynum % 10;
+        binarynum = binarynum / 10;
+        decimalnum = decimalnum + remainder*pow(2,temp);
+        temp++;
+    }
+    return decimalnum;
 }
 
 int main(){
 
+    int binarynum;
+    printf("Enter a binary number: ");
+    scanf("%ld", &binarynum);
+
+    printf("Equivalent decimal number is: %d", binaryToDecimal(binarynum));
 	return 0;
-	
+
 
 }
 
