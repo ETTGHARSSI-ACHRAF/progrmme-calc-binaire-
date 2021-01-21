37b746088f925a2426d28e1871789a4f535cf858
ETTGHARRSI-Achraf <achraf.ettgharssi1997@gmail.com>
2021-01-21 10:07:12 +0100

    add function

--- binaire programme.cpp
+++ binaire programme.cpp
@@ -1,4 +1,10 @@
 #include<stdio.h>
+//la function de calcule la somme de 2 binaire
+// resulta de type binaire
+int Somme(int bin1,int bin2){
+	int result;
+	return result;
+}
 int main(){
 	return 0;
 }
