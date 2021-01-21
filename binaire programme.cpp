56eb4d75dd3c5034f96cf047eeb1c86d6a515a2f
Mounir Zelouani <mounirzelouani10@gmail.com>
2021-01-21 14:03:12 +0100

    commit 3

--- binaire programme.cpp
+++ binaire programme.cpp
@@ -1,17 +1,56 @@
 #include<stdio.h>
+#include<string.h>
+#include <stdlib.h>
+
+//la function de calcule la somme de 2 binaire
+// resulta de type int
+int Somme(int bin1[4],int bin2[4]){
+
+	
+}
 
 
 /*first function*/
 /*function type INT*/
 
 
-int bin(){
-	
-	return bin;
+
+    
+     
+  
+int main(){  
+int bin[4],n,i;    
+system ("cls");  
+printf("Enter the number to convert: ");    
+scanf("%d",&n);    
+for(i=0;n>0;i++)    
+{    
+bin[i]=n%2;    
+n=n/2;    
+}    
+printf("\nBinary of Given Number is=");    
+for(i=i-1;i>=0;i--)    
+{    
+printf("%d",bin[i]);    
+}    
+return 0;  
+}  
+
+
+// fonction int
+// get int (biin)
+// return int (_innt)
+int bintoint (int biin)
+{
+    int _innt;
+
+    return _innt;
 }
 
+/*int main(){
 
-int main(){
 	return 0;
 	
+
 }
+*/
