cff2f83d1eb21c8fe46dea207150ac82cee163a2
ETTGHARRSI-Achraf <achraf.ettgharssi1997@gmail.com>
2021-01-21 15:51:19 +0100

    finale

--- binaire programme.cpp
+++ binaire programme.cpp
@@ -3,8 +3,51 @@
 
 //la function de calcule la somme de 2 binaire
 // resulta de type int
-int Somme(int bin1[4],int bin2[4]){
+int rest=0;
 
+
+int  sTw(int a,int b, int k)
+{
+    int r=0;
+    rest=0;
+    if(a+b+k==1)
+    {
+        r=1;
+    }
+
+    if(a+b+k==2)
+        {
+           rest=1;
+           r=0;
+        }else if(a+b+k==3)
+        {
+           rest=1;
+           r=1;
+        }
+
+    return r;
+}
+
+int Somme(int a[3],int b[3],int c[4]){
+    for (int i = 2;i >= 0;i--)
+    {
+        c[i+1]=sTw(a[i],b[i], rest);
+
+    }
+    if(rest==1)
+    {
+        c[0]=rest;
+
+    }
+    if(rest==1)
+    {
+        printf("%d",c[0]);
+    }
+    for (i = 1 ; i <4; i++)
+      {
+
+        printf("%d",c[i]);
+        }
 	
 }
 
